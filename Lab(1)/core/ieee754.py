40c7bf1a564aa63cc41b057cecb6f8afaf5354c8
Myrfi <188091935+Myrfi@users.noreply.github.com>
2026-06-07 23:28:13 +0300

    Update ieee754.py

--- Lab(1)/core/ieee754.py
+++ Lab(1)/core/ieee754.py
@@ -1,66 +1,48 @@
-from constants import TOTAL_BITS
+from struct import pack, unpack
 
 
-class IEEE754Float:
+class FloatCodec:
+
     @staticmethod
-    def encode(value):
+    def pack(x: float) -> int:
+        return unpack('>I', pack('>f', x))[0]
+
+    @staticmethod
+    def unpack(bits: int) -> float:
+        return unpack('>f', pack('>I', bits))[0]
+
+    @classmethod
+    def to_bits(cls, value: float) -> list:
         if value == 0.0:
-            return ["0"] * TOTAL_BITS
+            return ['0'] * 32
+        bits = cls.pack(value)
+        return [str((bits >> i) & 1) for i in range(31, -1, -1)]
 
-        sign = "1" if value < 0 else "0"
-        val = abs(value)
+    @classmethod
+    def from_bits(cls, bits: list) -> float:
+        if all(b == '0' for b in bits):
+            return 0.0
+        val = 0
+        for b in bits:
+            val = (val << 1) | (1 if b == '1' else 0)
+        return cls.unpack(val)
 
-        int_part = int(val)
-        frac_part = val - int_part
+    @classmethod
+    def add(cls, a: float, b: float) -> tuple:
+        res = a + b
+        return cls.to_bits(res), res
 
-        # Целая часть в биты
-        int_bits = []
-        while int_part > 0:
-            int_bits.insert(0, str(int_part % 2))
-            int_part //= 2
+    @classmethod
+    def sub(cls, a: float, b: float) -> tuple:
+        res = a - b
+        return cls.to_bits(res), res
 
-        # Дробная часть в биты
-        frac_bits = []
-        while frac_part > 0 and len(frac_bits) < 32:
-            frac_part *= 2
-            frac_bits.append(str(int(frac_part)))
-            frac_part -= int(frac_part)
+    @classmethod
+    def mul(cls, a: float, b: float) -> tuple:
+        res = a * b
+        return cls.to_bits(res), res
 
-        # Нормализация
-        if int_bits:
-            exponent = len(int_bits) - 1
-            mantissa = int_bits[1:] + frac_bits
-        else:
-            if "1" in frac_bits:
-                first_one = frac_bits.index("1")
-                exponent = -(first_one + 1)
-                mantissa = frac_bits[first_one + 1:]
-            else:
-                exponent = 0
-                mantissa = []
-
-        # Смещение экспоненты
-        exp_val = exponent + 127
-        exp_bits = ["0"] * 8
-        for i in range(7, -1, -1):
-            exp_bits[i] = "1" if exp_val % 2 != 0 else "0"
-            exp_val //= 2
-
-        mantissa_bits = (mantissa + ["0"] * 23)[:23]
-        return [sign] + exp_bits + mantissa_bits
-
-    @staticmethod
-    def add(a, b):
-        return IEEE754Float.encode(a + b), a + b
-
-    @staticmethod
-    def subtract(a, b):
-        return IEEE754Float.encode(a - b), a - b
-
-    @staticmethod
-    def multiply(a, b):
-        return IEEE754Float.encode(a * b), a * b
-
-    @staticmethod
-    def divide(a, b):
-        return IEEE754Float.encode(a / b), a / b
+    @classmethod
+    def div(cls, a: float, b: float) -> tuple:
+        res = a / b
+        return cls.to_bits(res), res
