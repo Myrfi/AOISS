1200b0795770b9d02b6ea5bf4323b73da8a273d2
Myrfi <188091935+Myrfi@users.noreply.github.com>
2026-06-07 23:27:08 +0300

    Update binary_codes.py

--- Lab(1)/core/binary_codes.py
+++ Lab(1)/core/binary_codes.py
@@ -1,45 +1,53 @@
+from typing import List
 from constants import TOTAL_BITS, SIGN_BIT_INDEX
 from core.utils import BitUtils
 
-class BinaryConverter:
-    @staticmethod
-    def decimal_to_binary(number):
-        """Перевод модуля числа в двоичный массив (делением на 2)"""
-        res = ["0"] * TOTAL_BITS
-        n = abs(number)
-        for i in range(TOTAL_BITS - 1, 0, -1):
-            res[i] = "1" if n % 2 != 0 else "0"
-            n //= 2
-        return res
 
-    @staticmethod
-    def to_direct_code(number):
-        bits = BinaryConverter.decimal_to_binary(number)
-        if number < 0:
-            bits[SIGN_BIT_INDEX] = "1"
+class Codec:
+
+    def __init__(self, word_size: int = TOTAL_BITS):
+        self._size = word_size
+        self._sign_pos = word_size - 1
+
+    def _abs_to_bits(self, value: int) -> List[str]:
+        result = ['0'] * self._size
+        num = abs(value)
+        for idx in range(self._size - 1, 0, -1):
+            result[idx] = '1' if (num & 1) else '0'
+            num >>= 1
+        return result
+
+    def to_direct(self, num: int) -> List[str]:
+        bits = self._abs_to_bits(num)
+        if num < 0:
+            bits[self._sign_pos] = '1'
         return bits
 
-    @staticmethod
-    def to_reverse_code(number):
-        direct = BinaryConverter.to_direct_code(number)
-        if number >= 0:
+    def to_inverse(self, num: int) -> List[str]:
+        direct = self.to_direct(num)
+        if num >= 0:
             return direct
-        return BitUtils.invert_bits(direct, 1)
+        return BitUtils.invert_bits(direct, start=1)
 
-    @staticmethod
-    def to_additional_code(number):
-        if number >= 0:
-            return BinaryConverter.to_direct_code(number)
-        reverse = BinaryConverter.to_reverse_code(number)
-        return BitUtils.add_one(reverse)
+    def to_complement(self, num: int) -> List[str]:
+        if num >= 0:
+            return self.to_direct(num)
+        inv = self.to_inverse(num)
+        return BitUtils.add_one(inv)
 
-    @staticmethod
-    def from_additional_code(bits):
-        """Перевод из доп. кода в 10-ное число (для проверки)"""
-        res = 0
-        if bits[SIGN_BIT_INDEX] == "1":
-            res = -(2 ** (TOTAL_BITS - 1))
-        for i in range(1, TOTAL_BITS):
-            if bits[i] == "1":
-                res += 2 ** (TOTAL_BITS - 1 - i)
-        return res
+    def from_complement(self, bits: List[str]) -> int:
+        if bits[self._sign_pos] == '0':
+            val = 0
+            for i in range(1, self._size):
+                if bits[i] == '1':
+                    val += 1 << (self._size - 1 - i)
+            return val
+        else:
+            inverted = ['1' if b == '0' else '0' for b in bits]
+            inverted[self._sign_pos] = '0'
+            plus_one = BitUtils.add_one(inverted)
+            val = 0
+            for i in range(1, self._size):
+                if plus_one[i] == '1':
+                    val += 1 << (self._size - 1 - i)
+            return -val
